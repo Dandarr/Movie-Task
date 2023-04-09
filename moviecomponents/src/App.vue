74fc7ec15742c02d8e477bcd74bd1c1344de038f
Dandarr <thomas.smith.6007@gmail.com>
2023-04-09 16:55:56 +0100

    a billion node modules and base files

---
+++ moviecomponents/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Movie Information</h1>
+    <MovieBox :movieData="movieData" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import MovieBox from './components/MovieComponent.vue'
+import axios from 'axios'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    MovieBox
+  },
+  data () {
+    return {
+      movieData: {} as Record<string, unknown>,
+      listMovie: []
+    }
+  },
+  mounted () {
+    axios.get('https://api.themoviedb.org/3/movie/550?api_key=c6daa157a2252e0b4426c4bf32d06641')
+      .then((response) => {
+        this.movieData = response.data
+        this.listMovie = response.data
+        console.log(this.movieData, this.listMovie)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+})
+</script>
---
+++ moviecomponents/src/components/MovieComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>{{ movieData.title }}</h1>
+        <img :src="'https://image.tmdb.org/t/p/w500' + movieData.poster_path" :alt="movieData.title + ' poster'">
+        <p><strong>Release Date:</strong> {{ movieData.release_date }}</p>
+        <p><strong>Runtime:</strong> {{ movieData.runtime }} minutes</p>
+        <p><strong>Genres:</strong> {{ getGenres(movieData.genres) }}</p>
+        <p><strong>Overview:</strong> {{ movieData.overview }}</p>
+        <p><strong>Production Companies:</strong></p>
+        <ul>
+            <li v-for="company in movieData.production_companies" :key="company.id">
+                {{ company.name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieData',
+  props: {
+    movieData: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    getGenres (genres) {
+      return genres.map(genre => genre.name).join(', ')
+    }
+  }
+}
+</script>
+
+<style>
+/* Add styles as desired */
+</style>
