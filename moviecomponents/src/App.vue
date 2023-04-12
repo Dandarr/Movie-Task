4873ffeff478eb56def0b2bb34ad17e4e1c5b742
Dandarr <thomas.smith.6007@gmail.com>
2023-04-12 19:23:52 +0100

    fixed MovieComponent, added styling

--- moviecomponents/src/App.vue
+++ moviecomponents/src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
-    <h1>Movie Information</h1>
-    <MovieBox :movieData="movieData" />
+    <h1 class="PageHeading">Movie Information</h1>
+    <MovieBox v-for="(movieData, index) in listMovie" :key="index" :movieData="movieData" />
   </div>
 </template>
-
 <script lang="ts">
 import { defineComponent } from 'vue'
 import MovieBox from './components/MovieComponent.vue'
@@ -17,16 +16,15 @@ export default defineComponent({
   },
   data () {
     return {
-      movieData: {},
-      listMovie: []
+      movieDataList: [],
+      listMovie: [155, 550, 286217, 152603]
     }
   },
   mounted () {
-    axios.get('https://api.themoviedb.org/3/movie/550?api_key=c6daa157a2252e0b4426c4bf32d06641')
-      .then((response) => {
-        this.movieData = response.data
-        this.listMovie = response.data
-        console.log(this.movieData, this.listMovie)
+    Promise.all(this.listMovie.map(movieId => axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=c6daa157a2252e0b4426c4bf32d06641`)))
+      .then((responses) => {
+        this.listMovie = responses.map(response => response.data)
+        console.log(this.listMovie)
       })
       .catch((error) => {
         console.log(error)
@@ -34,3 +32,31 @@ export default defineComponent({
   }
 })
 </script>
+<style>
+body{
+  text-align: left;
+  border: 0px;
+  padding: 0px;
+  background: #1E1E1E;
+  color: beige;
+  font-family: 'Helvetica Neue Light', 'HelveticaNeue-Light', 'Helvetica Neue', sans-serif;
+}
+
+@media (min-width: 1025px) {
+  body{
+    margin-left: 30%;
+    margin-right: 30%;
+    max-width: auto;
+  }
+}
+@media (max-width: 1024px) and (min-width: 512px){
+  body{
+    margin-left: 15%;
+    margin-right: 15%;
+    width: auto;
+  }
+}
+.PageHeading{
+  text-align: center;
+}
+</style>
--- moviecomponents/src/components/MovieComponent.vue
+++ moviecomponents/src/components/MovieComponent.vue
@@ -1,17 +1,36 @@
 <template>
-    <div>
-        <h1>{{ movieData.title }}</h1>
-        <img :src="'https://image.tmdb.org/t/p/w500' + movieData.poster_path" :alt="movieData.title + ' poster'">
-        <p><strong>Release Date:</strong> {{ movieData.release_date }}</p>
-        <p><strong>Runtime:</strong> {{ movieData.runtime }} minutes</p>
-        <p><strong>Genres:</strong> {{ getGenres(movieData.genres) }}</p>
-        <p><strong>Overview:</strong> {{ movieData.overview }}</p>
-        <p><strong>Production Companies:</strong></p>
-        <ul>
-            <li v-for="company in movieData.production_companies" :key="company.id">
-                {{ company.name }}
-            </li>
-        </ul>
+    <div class="MovieComponentContainer">
+        <h1 class="MovieName">{{ movieData.title }}</h1>
+        <img :src="'https://image.tmdb.org/t/p/w500' + movieData.poster_path" :alt="movieData.title + ' poster'"
+            class="MoviePoster">
+        <div class="MovieTextContainer">
+            <div>
+                <h2>Release Date:</h2>
+                <p>{{ movieData.release_date }}</p>
+            </div>
+            <div class="Runtime">
+                <h2>Runtime:</h2>
+                <p>{{ movieData.runtime }} minutes</p>
+            </div>
+            <!--
+            <div>
+                <h2>Genres:</h2>
+                <p>{{ getGenres(movieData.genres) }}</p>
+            </div>
+            -->
+            <div>
+                <h2>Overview:</h2>
+                <p>{{ movieData.overview }}</p>
+            </div>
+            <!--
+            <div>
+                <h2 class="MovieProducers">Production Companies:</h2>
+                <ul class="MovieList">
+                    <li v-for="company in movieData.production_companies" :key="company.id">{{ company.name }}</li>
+                </ul>
+            </div>
+            -->
+        </div>
     </div>
 </template>
 
@@ -25,13 +44,56 @@ export default {
     }
   },
   methods: {
-    getGenres (genres) {
-      return genres.map(genre => genre.name).join(', ')
-    }
+    // getGenres (genres) {
+    //   return genres.map(genre => genre.name).join(', ')
+    // }
   }
 }
 </script>
 
 <style>
-/* Add styles as desired */
+.MovieList {
+    text-align: left;
+    list-style-type: square;
+}
+h2{
+    font-size: 16px;
+}
+h2,
+p {
+    vertical-align: middle;
+    line-height: 24px;
+    height: fit-content;
+    margin-left: 15%;
+    display: flex;
+    margin-left: 265px;
+    width:480px;
+}
+
+.MoviePoster {
+    max-width: 250px;
+    position: absolute;
+    margin-top:0px ;
+}
+
+.MovieComponentContainer {
+    border: 4px solid beige;
+    border-radius: round 2px;
+    border-left: none;
+    border-right: none;
+    border-top: none;
+}
+
+.MovieTextContainer {
+    max-width: 40%;
+    display: inline-block;
+    margin-bottom: 7vh;
+}
+.MovieName{
+    text-align: left;
+    margin-left: 265px;
+}
+.MovieProducers{
+    text-align: left;
+}
 </style>
